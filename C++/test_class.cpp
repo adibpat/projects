4c730dbc8ec1abcfc12f62efd3e7937f46d297c9
Aditya Patwardhan <abpatwar@ncsu.edu>
2018-03-18 23:28:45 -0400

    Class object as member

--- C++/test_class.cpp
+++ C++/test_class.cpp
@@ -5,16 +5,64 @@
 
 using namespace std;
 
+string occupation_list[] =
+    {
+        "Unemployed",
+        "Engineer",
+        "Doctor",
+        "Teacher",
+    };
 
-void add_dmy (Date& date, int d, int m, int y)
+class Person {
+public:
+    string get_name() const;
+    // string get_occupation() const;
+    void get_birthdate(Date&) const;
+    // void set_name(string n);
+    // void set_occupation(int oid);
+    // void set_birthdate(Date&);
+    Person(const string, int, int, int, int);
+private:
+    string name;
+    Date birth_date;
+    string occupation;
+};
+
+/*
+ * Class Objects as members example
+ * See how we call constructor for member objects
+ */
+Person::Person(const string p_name, int d, int m, int y, int occ_id)
+    : birth_date(d,m,y)
 {
-    date.add_day(d).add_month(m).add_year(y);
+    name = p_name;
+    occupation = occupation_list[occ_id];
 }
 
-int main ()
+string Person::get_name() const
 {
-    //string test = month[1];
-    //cout << test << endl;
+    return (name);
+}
+
+void Person::get_birthdate(Date& date) const
+{
+    date = birth_date;
+    return;
+}
+
+void add_dmy (Date&, int, int, int);
+
+void test_class_person()
+{
+    Person p = Person("Aditya", 15, 9, 1992, 1);
+    cout << "P object's name is " << p.get_name() << endl;
+    Date p_bd;
+    p.get_birthdate(p_bd);
+    cout << "P's bday day is "<< p_bd.get_day() << endl;
+}
+
+void test_class_date()
+{
     Date my_bday;
     cout << "My birth date is ";
     my_bday.print_date();
@@ -41,5 +89,16 @@ int main ()
     copy_of_my_bday.print_date();
     cout << "Month in string is " << my_bday.get_string() << endl;
     copy_of_my_bday = sha_bday;
+}
+    
+int main ()
+{
+    //test_class_date();
+    test_class_person();
     return (0);
+}
+
+void add_dmy (Date& date, int d, int m, int y)
+{
+    date.add_day(d).add_month(m).add_year(y);
 }
