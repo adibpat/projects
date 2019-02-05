ed1c4494d57593984ed52e290ad1527b7618672f
Aditya Patwardhan <adibpat@gmail.com>
2019-02-04 22:36:08 -0500

    04Feb final

--- algo/greedy/activity_select.c
+++ algo/greedy/activity_select.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../sort/my_sort.h"
 
 void select_activity(int arr_size, int *start_arr, int *finish_arr)
 {
     // todo
 }
 
+int minimum_product_subset (int *arr, int arr_size)
+{
+    int negative = 0, product = 1, max_neg = 0;
+    quick_sort_driver(arr_size, arr);
+    for (int i = 0; i < arr_size; i++) {
+        if (arr[i] < 0) {
+            negative++;
+            max_neg = (arr[i] < max_neg) ? arr[i] : max_neg;
+        }
+        if (arr[i] == 0) return (0);
+    }
+    if (negative%2) {
+        for (int i = 0; i < arr_size; i++) {
+            product *= arr[i];
+        }
+        return (product);
+    } else if (negative > 0) {
+        for (int i = 0; i < arr_size; i++) {
+            product *= arr[i];
+        }
+        return (product/max_neg);
+    }
+    return (arr[0]);
+}
+
+int maximum_product_subset (int *arr, int arr_size)
+{
+    //todo
+}
+
 int main() {
-	int tc;
-	scanf("%d", &tc);
-	for (int i = 0; i < tc; i++) {
-	    int arr_size, *start_arr = NULL,*finish_arr = NULL;
-	    scanf("%d", &arr_size);
-	    start_arr = malloc(sizeof(int)*arr_size);
-	    finish_arr = malloc(sizeof(int)*arr_size);
-        for (int j = 0; j < arr_size; j++) {
-            scanf("%d ", &start_arr[j]);
-        }
-        for (int j = 0; j < arr_size; j++) {
-            scanf("%d ", &finish_arr[j]);
-        }
-        select_activity(arr_size, start_arr, finish_arr);
-	}
+    /* 1. Minimum product subset of an array */
+    int arr[] = {-1, 1, 2, -4, 3};
+    printf("MinPS is %d\n", minimum_product_subset(arr, 5));
+    printf("MaxPS is %d\n", maximum_product_subset(arr, 5));
+
 	return 0;
 }
